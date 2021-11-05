6ab74c1f95b77601801947d063ace42059c5de67
troy wigton <FlightTest@users.noreply.github.com>
2021-11-05 01:07:27 -0500

    Cleaned up some indentations

--- CPU_Consumer/threadMain.cpp
+++ CPU_Consumer/threadMain.cpp
@@ -7,54 +7,50 @@ using namespace std;
 mutex g_mtx;   // Is this an appropriate place to define a mutex?  Is my usage below appropriate?
 bool g_stopFlag = false;
 
+void foo(unsigned int msDelay)
+{
+    bool fast = false;
+    unsigned int sleep_ms;
+    unsigned long long int countModulo;
+    static unsigned int thisCallNumberStatic = 0;
+    unsigned int thisCallNumberAuto;
+    unsigned long long int loopCounter = 0ULL;
 
+    Sleep(msDelay);
 
+    // Try to prove that calls to stand alone foo retain their own local data within each thread.
+    g_mtx.lock();
+    cout << "Foo Call Number: " << ++thisCallNumberStatic << endl;
+    thisCallNumberAuto = thisCallNumberStatic;
+    g_mtx.unlock();
 
-
-    void foo(unsigned int msDelay)
+    if (fast)
     {
-        bool fast = false;
-        unsigned int sleep_ms;
-        unsigned long long int countModulo;
-        static unsigned int thisCallNumberStatic = 0;
-        unsigned int thisCallNumberAuto;
-        unsigned long long int loopCounter = 0ULL;
-
-        Sleep(msDelay);
-
-        // Try to prove that calls to stand alone foo retain their own local data within each thread.
-        g_mtx.lock();
-        cout << "Foo Call Number: " << ++thisCallNumberStatic << endl;
-        thisCallNumberAuto = thisCallNumberStatic;
-        g_mtx.unlock();
-
-        if (fast)
-        {
-            sleep_ms = 0; //zero for max CPU usage
-            countModulo = 100000000ULL;
-        }
-        else
-        {
-            sleep_ms = 1; // 1ms ootherwise, consumes less CPU
-            countModulo = 100ULL;
-        }
-
-       
-        while (g_stopFlag != true)
-        {
-            if (sleep_ms != 0) Sleep(sleep_ms); //Avoiding Sleep() overhead
-
-            if (++loopCounter % countModulo == 0ULL)
-            {
-                g_mtx.lock();
-                cout << "Foo #"<<thisCallNumberAuto<<": Loop Count = " << loopCounter << endl;
-                g_mtx.unlock();
-            }
-        }
-        g_mtx.lock();
-        cout <<"Foo # "<<thisCallNumberAuto<<" stopFlag detected" << endl;
-        g_mtx.unlock();
+        sleep_ms = 0; //zero for max CPU usage
+        countModulo = 100000000ULL;
     }
+    else
+    {
+        sleep_ms = 1; // 1ms ootherwise, consumes less CPU
+        countModulo = 100ULL;
+    }
+
+   
+    while (g_stopFlag != true)
+    {
+        if (sleep_ms != 0) Sleep(sleep_ms); //Avoiding Sleep() overhead
+
+        if (++loopCounter % countModulo == 0ULL)
+        {
+            g_mtx.lock();
+            cout << "Foo #"<<thisCallNumberAuto<<": Loop Count = " << loopCounter << endl;
+            g_mtx.unlock();
+        }
+    }
+    g_mtx.lock();
+    cout <<"Foo # "<<thisCallNumberAuto<<" stopFlag detected" << endl;
+    g_mtx.unlock();
+}
 
 void printStopMessage(void)
 {
@@ -83,7 +79,6 @@ int main(void)
     thread stopTriggerDetector(detectStopSignal,&g_stopFlag);
     startupMessage(3);
 
-   
     thread t1(foo,0);
     thread t2(foo,2000);
     thread t3(foo,4000);
